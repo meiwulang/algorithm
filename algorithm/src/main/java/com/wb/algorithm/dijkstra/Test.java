a8da25aa225e133d83b67973319579efafec5867
wb <418180062@qq.com>
2017-12-20 14:15:16 +0800

    修改格式

--- algorithm/src/main/java/com/wb/algorithm/dijkstra/Test.java
+++ algorithm/src/main/java/com/wb/algorithm/dijkstra/Test.java
@@ -25,22 +25,18 @@ public class Test {
 		vertexs.add(e);
 		vertexs.add(f);
 		int[][] edges = {
-				{ Integer.MAX_VALUE, 6, 3, Integer.MAX_VALUE, Integer.MAX_VALUE,
-						Integer.MAX_VALUE },
-				{ 6, Integer.MAX_VALUE, 2, 5, Integer.MAX_VALUE,
-						Integer.MAX_VALUE },
-				{ 3, 2, Integer.MAX_VALUE, 3, 4, Integer.MAX_VALUE },
-				{ Integer.MAX_VALUE, 5, 3, Integer.MAX_VALUE, 5, 3 },
-				{ Integer.MAX_VALUE, Integer.MAX_VALUE, 4, 5, Integer.MAX_VALUE,
-						5 },
-				{ Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 3, 5,
-						Integer.MAX_VALUE }
+				{ Integer.MAX_VALUE,                6,                3, Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE },
+				{ 6                ,Integer.MAX_VALUE,                2,                 5,Integer.MAX_VALUE,Integer.MAX_VALUE },
+				{                 3,                2,Integer.MAX_VALUE,                 3,                4,Integer.MAX_VALUE },
+				{ Integer.MAX_VALUE,                5,                3, Integer.MAX_VALUE,                5,                3 },
+				{ Integer.MAX_VALUE,Integer.MAX_VALUE,                4,                 5,Integer.MAX_VALUE,                5 },
+				{ Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,                 3,                5,Integer.MAX_VALUE }
 
 		};
 		Graph graph = new Graph(vertexs, edges);
 		graph.printGraph();
 		graph.search();
-		System.out.println(graph.getDistance(a, c));
+		//如何把任意两点的最短路径的轨迹找出来
 	}
 
 }
